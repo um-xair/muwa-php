d6362f5d6fdc8201dcb4b9d948154257e4c6c703
Alyssa <natasyahebat@gmail.com>
2024-12-02 15:17:32 +0800

    2-12-24

---
+++ footer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <title>Muwababy</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+        ::-webkit-scrollbar {
+            display: none;
+        }
+    </style>
+</head>
+<body class="bg-white mx-auto">
+<footer class="bg-[#CCB9AA] p-6 py-20 mt-20">
+    <div class="grid md:grid-cols-1 lg:grid-cols-3 gap-8 items-center">
+        <div class="flex flex-col justify-center items-center">
+            <img src="assets/muwa-removebg-preview.png" alt="Logo" class="w-3/5 h-auto">
+            <span class="mt-4 text-black text-sm font-medium">202301051637 (1545551-X)</span>
+        </div>
+
+        <div class="flex flex-col sm:flex-row sm:space-x-6 justify-center items-center">
+            <a href="index.html" class="text-black">Home</a>
+            <a href="about.html" class="text-black">About</a>
+            <a href="category.html" class="text-black">Stroller</a>
+            <a href="accessories.html" class="text-black">Accessories</a>
+            <a href="privacy-policy.html" class="text-black">Support</a>
+            <a href="contact-us.html" class="text-black">Contact</a>
+        </div>
+
+        <div class="flex space-x-6 justify-center items-center">
+            <a href="#" class="bg-[#844D3E] text-white p-3 rounded-full w-12 h-12 flex justify-center items-center">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="bg-[#844D3E] text-white p-3 rounded-full w-12 h-12 flex justify-center items-center">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#" class="bg-[#844D3E] text-white p-3 rounded-full w-12 h-12 flex justify-center items-center">
+                <i class="fab fa-tiktok"></i>
+            </a>
+            <a href="#" class="bg-[#844D3E] text-white p-3 rounded-full w-12 h-12 flex justify-center items-center">
+                <i class="fab fa-youtube"></i>
+            </a>
+        </div>
+    </div>
+
+    <div class="mt-6 text-center text-black font-semibold">
+        <p>© 2024 | Muwababy All Rights Reserved.</p>
+    </div>
+</footer>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,216 @@
+<?php
+include 'header.php';
+?>
+
+<main class="px-10 md:px-[6%] mt-20 grid grid-cols-1 md:grid-cols-2 gap-8 md:min-h-[70vh]">
+        <div class="flex flex-col justify-center space-y-4 md:space-y-6">
+            <h2 class="text-4xl lg:text-8xl md:text-6xl font-bold">Modern Stylish For Modern Parenting</h2>
+            <p class="text-black w-full lg:w-3/4 text-sm text-justify">Upgrade your baby gear with our modern stylish strollers with Muwababy. 
+                Find strollers that are not only practical but also fashionable, ensuring your baby travels in style</p>
+            <button class="bg-[#844D3E] text-lg text-white px-3 py-3 md:py-5 rounded-[20px] lg:w-2/4">Explore Modern Stroller</button>
+        </div>
+
+        <div class="flex h-full w-full items-center justify-center">
+            <div class="grid h-full w-full gap-4 grid-cols-5 grid-rows-4">
+                <div class="col-span-5 row-span-2 flex items-center justify-center">
+                    <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/97176-02913_header_web__(5).png" class="w-full h-full object-cover rounded-[30px]">
+                </div>
+                <div class="col-span-2 row-span-2 flex items-center justify-center h-4/5">
+                    <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/66311-87814_gezin-bij-zee-450x300.jpg" class="w-full h-full object-cover rounded-[30px]">
+                </div>
+                <div class="col-span-3 row-span-2 flex items-center justify-center h-4/5">
+                    <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/66207-11634_inluencer-met-accessoires-450x300.jpg" class="w-full h-full object-cover rounded-[30px]">
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <section class="px-10 md:px-[6%] flex flex-col items-center justify-center mt-20 mb-20">
+        <div class="w-full h-[80vh] bg-black rounded-[30px] overflow-hidden relative">
+            <iframe 
+                src="https://www.youtube.com/embed/HFbqUnqFFXA?si=dfxXut1uuy4OTZcs" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen 
+                class="w-full h-full"
+            ></iframe>
+        </div>
+    </section>
+
+    <section class="px-10 md:px-[6%] min-h-[80vh] flex items-center justify-center mt-20 mb-20">
+        <div class="w-full flex flex-col md:flex-row gap-8">
+            <div class="flex-1 mb-8 md:mb-0">
+                <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/95267-18655_Untitled_design_(1).png" alt="Image" class="w-full h-auto rounded-[30px]">
+            </div>
+    
+            <div class="flex-1 flex flex-col justify-center space-y-4 text-justify">
+                <p class="text-sm text-black font-semibold">About Us</p>
+                <h2 class="text-4xl md:text-6xl font-bold">Smart Solutions</h2>
+                <p class="text-black text-sm mb-4">Muwababy was created to give parents more freedom of movement. We believe in smart solutions that make life easier. Problems are meant to be solved, and that's what our brand focuses on.</p>
+                <p class="text-black text-sm mb-4">Being a parent often limits your freedom, but Muwababy aims to remove those limitations. Our mission is to help parents move freely and easily with their little ones.</p>
+    
+                <button class="bg-[#844D3E] text-lg text-white px-3 py-3 md:py-5 rounded-[20px] lg:w-2/4">Read about Muwa</button>
+            </div>
+        </div>
+    </section>
+    
+    <section class="px-10 md:px-[6%] min-h-[80vh] flex items-center justify-center mt-20 mb-20">
+        <div class="w-full max-w-screen-lg mx-auto relative md:mt-10 mt-4">
+            <h2 class="text-4xl md:text-6xl font-bold mb-8 text-center">Our Most Favourite</h2>
+    
+            <div class="swiper mb-20">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide flex-shrink-0 w-full md:w-1/3 bg-white overflow-hidden">
+                        <img src="assets/muwa-white.png" alt="Stroller" class="w-full h-auto object-cover">
+                        <div class="p-4">
+                            <p class="text-lg font-medium text-black font-bold">Muwababy Horsi Stroller (White)</p>
+                            <div class="flex justify-between items-center mt-2">
+                                <p class="text-sm text-black line-through">RM1399</p>
+                                <p class="text-3xl font-bold text-black">RM799</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide flex-shrink-0 w-full md:w-1/3 bg-white overflow-hidden">
+                        <img src="assets/muwa-brown.png" alt="Stroller" class="w-full h-full object-cover">
+                        <div class="p-4">
+                            <p class="text-lg font-medium text-black font-bold">Muwababy Horsi Stroller (Brown)</p>
+                            <div class="flex justify-between items-center mt-2">
+                                <p class="text-sm text-black line-through">RM1399</p>
+                                <p class="text-3xl font-bold text-black">RM799</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide flex-shrink-0 w-full md:w-1/3 bg-white overflow-hidden">
+                        <img src="assets/muwa-black.png" alt="Stroller" class="w-full h-full object-cover">
+                        <div class="p-4">
+                            <p class="text-lg font-medium text-black font-bold">Muwababy Horsi Stroller (Black)</p>
+                            <div class="flex justify-between items-center mt-2">
+                                <p class="text-sm text-black line-through">RM1399</p>
+                                <p class="text-3xl font-bold text-black">RM799</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide flex-shrink-0 w-full md:w-1/3 bg-white overflow-hidden">
+                        <img src="assets/muwa-brown.png" alt="Stroller" class="w-full h-full object-cover">
+                        <div class="p-4">
+                            <p class="text-lg font-medium text-black font-bold">Muwababy Horsi Stroller (Brown)</p>
+                            <div class="flex justify-between items-center mt-2">
+                                <p class="text-sm text-black line-through">RM1399</p>
+                                <p class="text-3xl font-bold text-black">RM799</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <button id="prevButton" class="absolute -left-20 top-1/2 transform -translate-y-1/2 bg-[#844D3E] p-3 rounded-full z-[999] lg:block hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
+                    <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
+                </svg>
+            </button>
+            <button id="nextButton" class="absolute -right-20 top-1/2 transform -translate-y-1/2 bg-[#844D3E] p-3 rounded-full z-[999] lg:block hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
+                    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
+                </svg>
+            </button>
+        </div>
+    </section>
+    
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        const swiper = new Swiper('.swiper', {
+            loop: true,
+            grabCursor: true,
+            spaceBetween: 30,
+            slidesPerView: 3, 
+            breakpoints: {
+                0: {
+                    slidesPerView: 1 
+                },
+                768: {
+                    slidesPerView: 2 
+                },
+                1024: {
+                    slidesPerView: 3
+                }
+            },
+            navigation: {
+                nextEl: '#nextButton',
+                prevEl: '#prevButton',
+            }
+        });
+    </script>
+
+    <section class="px-10 md:px-[6%] flex flex-col items-center justify-center mt-20 mb-20">
+        <div class="w-full h-[80vh] bg-gray-200 rounded-[50px] overflow-hidden relative">
+            <img 
+                src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/97176-02913_header_web__(5).png" 
+                alt="Image Description" 
+                class="w-full h-full object-cover"
+            />
+        </div>
+    </section>
+
+    <section class="px-10 md:px-[6%] min-h-[80vh] grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-20 mb-20">
+        <div class="flex h-4/5 w-full items-center justify-center">
+            <div class="grid h-full w-full gap-4 p-2 grid-cols-4 grid-rows-4">
+                <div class="col-span-2 row-span-2 flex items-center justify-center">
+                    <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/95850-48533_Visuals_website_(4).png" alt="Salmon" class="h-full w-full object-cover rounded-[30px]" />
+                </div>
+                <div class="col-span-1 row-span-2 flex items-center justify-center">
+                    <p></p>                
+                </div>
+                <div class="col-span-1 row-span-2 flex items-center justify-center">
+                    <p></p>                
+                </div>
+                <div class="col-span-1 row-span-2 flex items-center justify-center">
+                    <p></p>                
+                </div>
+                <div class="col-span-1 row-span-2 flex items-center justify-center">
+                    <p></p>                
+                </div>
+                <div class="col-span-2 row-span-2 flex items-center justify-center">
+                    <img src="https://assets.nextchapter-ecommerce.com/Media/g8h9xbw9/95850-48533_Visuals_website_(4).png" alt="Tomato" class="h-full w-full object-cover rounded-[30px]" />
+                </div>
+            </div>
+        </div>
+      
+        <div class="flex flex-col justify-center space-y-4 md:space-y-6 md:ml-14">
+            <p class="text-black w-full md:w-3/4 text-sm md:text-lg font-semibold">The Muwababy Stroller</p>
+            <h2 class="text-4xl md:text-6xl font-bold text-left">Comfort and Innovation</h2>
+            <p class="text-black w-full text-sm md:text-base text-justify md:text-left">Discover the perfect blend of style, functionality, and safety with Muwababy strollers.</p>            
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full mt-8 p-4">
+                <div class="flex flex-col items-center text-center p-4">
+                    <div class="mb-4">
+                        <i class="fas fa-globe text-6xl"></i>
+                    </div>
+                    <p class="text-4xl font-bold">200+</p>
+                    <p class="text-center text-black text-sm">Strollers Sold Worldwide</p>
+                </div>
+                <div class="flex flex-col items-center text-center p-4">
+                    <div class="mb-4">
+                        <i class="fas fa-star text-6xl"></i>
+                    </div>
+                    <p class="text-4xl font-bold">5/5</p>
+                    <p class="text-center text-black text-sm">Top-Rated by Parents</p>
+                </div>
+                <div class="flex flex-col items-center text-center p-4">
+                    <div class="mb-4">
+                        <i class="fas fa-store text-6xl"></i>
+                    </div>
+                    <p class="text-4xl font-bold">80%</p>
+                    <p class="text-center text-black text-sm">Sold on Physical Stores</p>
+                </div>
+            </div>
+        </div>
+        
+        
+    </section>
+
+
+    <?php
+// Include the footer
+include 'footer.php';
+?>
