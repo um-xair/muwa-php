be2f64bb560e12dd8255fd9710981d4d3cfebea4
Alyssa <natasyahebat@gmail.com>
2024-12-06 14:41:51 +0800

    6-12-24

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -73,7 +73,7 @@
                     </ul>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="flex items-center block py-3 px-4 hover:bg-[#614EF7] hover:text-white rounded-[30px] group">
+                    <a href="order.php" class="flex items-center block py-3 px-4 hover:bg-[#614EF7] hover:text-white rounded-[30px] group">
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" class="fill-black group-hover:fill-white">
                             <path d="M200-80q-33 0-56.5-23.5T120-160v-451q-18-11-29-28.5T80-680v-120q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v120q0 23-11 40.5T840-611v451q0 33-23.5 56.5T760-80H200Zm0-520v440h560v-440H200Zm-40-80h640v-120H160v120Zm200 280h240v-80H360v80Zm120 20Z"/>
                         </svg>
